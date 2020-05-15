d7f8296006617cd534436d8cf81a4b8e2a610485
RobertsFerreira <robertsilva-ferreira@hotmail.com>
2020-05-14 23:48:26 -0300

    errorPg file updated - add styleText

--- photomasterFlutter/lib/pages/error_page.dart
+++ photomasterFlutter/lib/pages/error_page.dart
@@ -5,31 +5,56 @@ class ErrorPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Center(
-              child: Text("Erro ao carregar, verifique sua conexão!"),
+        body: Stack(
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.fromLTRB(10, 20, 10, 20),
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              stops: [0.1, 1.2],
+              colors: [
+                Color(0xFFF92B7F),
+                Color(0xFFF58524),
+              ],
             ),
           ),
-          IconButton(
-            icon: Icon(
-              Icons.refresh,
-              size: 25,
-            ),
-            onPressed: () {
-              Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => Splash(),
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Center(
+                child: Text(
+                  "Erro ao carregar, verifique sua conexão!",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              );
-            },
-          ),
-        ],
-      ),
-    );
+              ),
+            ),
+            IconButton(
+              icon: Icon(
+                Icons.refresh,
+                size: 35,
+                color: Colors.white,
+              ),
+              onPressed: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => Splash(),
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ],
+    ));
   }
 }
